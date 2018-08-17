5a1628f65f07a4f3f13457507c05dccd7f0e342d
Yomar Miranda <mirandayomar@gmail.com>
2018-08-17 13:22:40 -0500

    Bloqueando las rutas protegidas por medio de un Guard implementando la interface CanActivate.

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+    constructor(private _auth: AuthService) { }
+
+    canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if (this._auth.isAuthenticated()) {
+            console.log('Puede ingresar');
+            return true;
+        } else {
+            console.error('No puede ingresar, bloqueado por el Guard');
+            return false;
+        }
+    }
 }

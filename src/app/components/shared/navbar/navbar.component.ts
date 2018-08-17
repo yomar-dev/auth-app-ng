a6ef1b4a74f404eb7f9cf609c1adf7001b50e76c
Yomar Miranda <mirandayomar@gmail.com>
2018-08-17 12:58:09 -0500

    Permitir iniciar y cerrar sesión a través de Auth0.

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html'
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-    constructor(private _auth: AuthService) { }
-
-    ngOnInit() {
+    constructor(private _auth: AuthService) {
+        this._auth.handleAuthentication();
     }
 
     login() {
         this._auth.login();
+    }
+
+    logout() {
+        this._auth.logout();
     }
 
 }

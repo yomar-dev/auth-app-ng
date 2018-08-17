856c8be461db0ad32e676aefa11f60861a1f1610
Yomar Miranda <mirandayomar@gmail.com>
2018-08-17 12:36:02 -0500

    Configurando el servicio Auth con la información que me brinda Auth0.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+// src/app/auth/auth.service.ts
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import * as auth0 from 'auth0-js';
+
+(window as any).global = window;
+
+@Injectable()
 export class AuthService {
 
-  constructor() { }
+    auth0 = new auth0.WebAuth({
+        clientID: 'xN0YtGWlZLmufiJQwDoWiJ0DJb8rSpXR',
+        domain: 'yomar-dev.auth0.com',
+        responseType: 'token id_token',
+        audience: 'https://yomar-dev.auth0.com/userinfo',
+        redirectUri: 'http://localhost:4200/callback',
+        scope: 'openid'
+    });
+
+    constructor(public router: Router) { }
+
+    public login(): void {
+        this.auth0.authorize();
+    }
 }
